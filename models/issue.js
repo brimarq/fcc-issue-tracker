7e4811dd5f97398a97202852de48d935b4562ba7
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-30 20:13:38 -0500

    Add getPublicFields method to schema.

--- models/issue.js
+++ models/issue.js
@@ -7,13 +7,29 @@ const IssueSchema = new Schema(
     issue_text: {type: String, trim: true, required: true},
     project: {type: String, trim: true, required: true},
     created_by: {type: String, trim: true, required: true},
-    assigned_to: {type: String, trim: true},
-    status_text: {type: String, trim: true},
+    assigned_to: {type: String, default: ''},
+    status_text: {type: String, default: ''},
     open: {type: Boolean, default: true}
   }, 
   {
-    timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' }
+    timestamps: { createdAt: 'created_on', updatedAt: 'updated_on' },
+    minimize: false
   }
 );
+
+IssueSchema.methods.getPublicFields = function () {
+  const returnObject = {
+    _id: this._id,
+    issue_title: this.issue_title,
+    issue_text: this.issue_text,
+    created_by: this.created_by,
+    assigned_to: this.assigned_to,
+    status_text: this.status_text,
+    open: this.open,
+    created_on: this.created_on,
+    updated_on: this.updated_on
+  };
+  return returnObject;
+};
 
 module.exports = mongoose.model('Issue', IssueSchema);
